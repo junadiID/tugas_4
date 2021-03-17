97cd45afa48b7516b487cbee1f0c1e46b1183d64
Junjun Junaedi <79783334+junadiID@users.noreply.github.com>
2021-03-17 21:53:51 +0700

    Add files via upload

---
+++ tugas.java
@@ -0,0 +1,23 @@
+import static java.lang.Thread.sleep;
+
+/*
+Nama    : Junjun Junaedi
+Nim     : 20190040028
+Matkul  : Sistem Operasi
+*/
+
+public class tugas {
+
+    public static void tugas(String[] args) throws InterruptedException {
+        
+        for(int i = 1; i<=50; i++){
+            Thread.sleep (1000);
+            System.out.println (i);
+        }
+        for(int j = 49; j>=1; j--){
+            Thread.sleep (1000);
+            System.out.println (j);
+        }
+    }
+
+}
